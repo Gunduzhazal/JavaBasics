a9906da96778b0da5431a834454640f2d50eba1f
Gunduzhazal <gunduzhazal90@gmail.com>
2022-10-01 19:48:10 -0500

    first commit

---
+++ src/JavaBasics/JavaBasics.java
@@ -0,0 +1,15 @@
+package JavaBasics;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i = 2; i < 10; i = i*i) {
+			   System.out.println(i);
+			}
+			
+
+	}
+
+}
